df728d7d761312be11dd12624ce5e6b9e0e8c539
FlaskNinja <usmanmariam43@gmail.com>
2025-03-21 18:48:30 +0100

    Added via upload
    nothing for now

---
+++ Alphabet/app2.py
@@ -0,0 +1,9 @@
+# from flask import Flask, render_template, redirect, url_for, flash, request
+# from app import users, sign_up
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = '0123456789ABC'
+
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
